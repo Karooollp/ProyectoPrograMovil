d81b6c8103875c00976cc6d80e3633035d002184
Karoll Alberto <misaperez05@unitec.edu>
2026-05-06 17:57:58 -0600

    CustomButton
    Implementando e implementando  un componente CustomButton

--- MiPrimeraApp/App.tsx
+++ MiPrimeraApp/App.tsx
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CustomButton from './src/components/CustomButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Primera App Movil de Misap</Text>
       <StatusBar style="auto" />
+     <CustomButton
+      title={"App"} 
+      onPress={()=>{
+        console.log("Press desde boton App");
+
+     }}
+     />
+
+     <CustomButton
+      title={"Secundary Button"} 
+      onPress={()=>{
+        console.log("Press desde boton se secundario");
+
+     }}
+     />
+     
     </View>
   );
 }
